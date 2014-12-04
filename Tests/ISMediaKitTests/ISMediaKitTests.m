17212cca768364948c053cc4aa88b9fe876bb295
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-12-04 08:39:08 +0100

    Adding the first tests.

--- Tests/ISMediaKitTests/ISMediaKitTests.m
+++ Tests/ISMediaKitTests/ISMediaKitTests.m
@@ -8,33 +8,83 @@
 
 #import <Cocoa/Cocoa.h>
 #import <XCTest/XCTest.h>
+#import <ISMediaKit/ISMediaKit.h>
 
 @interface ISMediaKitTests : XCTestCase
+
+@property (nonatomic, readwrite, strong) ISMKDatabaseClient *client;
 
 @end
 
 @implementation ISMediaKitTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)configureDatabaseClient:(NSString *)path
+{
+    // TODO Movie this into a loadConfiguration: call.
+    
+    // Check the configuration exists.
+    NSString *configurationPath = [path stringByExpandingTildeInPath];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:configurationPath]) {
+        XCTFail(@"Configuration file not found at '%@'.\n", configurationPath);
+    }
+    
+    // Load the configuration.
+    NSDictionary *configuration = [NSDictionary dictionaryWithContentsOfFile:configurationPath];
+    if (configuration == nil) {
+        XCTFail(@"Unable to load configuration file at '%@'.\n", configurationPath);
+    }
+    
+    // Check the tvdb-api-key exists.
+    NSString *tvdbAPIKey = configuration[@"tvdb-api-key"];
+    if (tvdbAPIKey == nil) {
+        XCTFail(@"Unable to find 'tvdb-api-key' in the configuration file.\n");
+    }
+    
+    // Check the mdb-api-key exists.
+    NSString *mdbAPIKey = configuration[@"mdb-api-key"];
+    if (mdbAPIKey == nil) {
+        XCTFail(@"Unable to find 'mdb-api-key' in the configuration file.\n");
+    }
+
+    // Configure the client.
+    [self.client setTVDBAPIKey:tvdbAPIKey
+                     mdbAPIKey:mdbAPIKey];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)setUp
+{
+    [super setUp];
+    self.client = [ISMKDatabaseClient sharedInstance];
+    XCTAssertNotNil(self.client, @"Unable to get the shared client instance");
+    // TODO This currently shares the configuration file with add-to-itunes and needs to be changed.
+    [self configureDatabaseClient:@"~/.add-to-itunes.plist"];
+}
+
+- (void)tearDown
+{
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (NSDictionary *)searchForFilename:(NSString *)filename
+{
+    __block NSDictionary *result = nil;
+    dispatch_semaphore_t sem = dispatch_semaphore_create(0);
+    [self.client searchWithFilename:filename completionBlock:^(NSDictionary *media) {
+        result = media;
+        dispatch_semaphore_signal(sem);
+    }];
+    dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
+    return result;
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testArrowS01E01
+{
+    NSDictionary *media = [self searchForFilename:@"arrow.s01e01.m4v"];
+    XCTAssertEqualObjects(media[ISMKKeyShowTitle], @"Arrow");
+    XCTAssertEqualObjects(media[ISMKKeyEpisodeTitle], @"Pilot");
+    XCTAssertEqualObjects(media[ISMKKeyEpisodeSeason], @1);
+    XCTAssertEqualObjects(media[ISMKKeyEpisodeNumber], @1);
 }
 
 @end
