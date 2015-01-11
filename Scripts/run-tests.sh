94a7167f1948c7279817dcc90110bec1486435a6
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-01-11 22:40:31 +0000

    Cleaning up the test runner.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -7,7 +7,9 @@ script_directory=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 root_directory="$script_directory/.."
 tests=ISMediaKitTests
 
-pushd "$root_directory/Tests"
-pod update
-xcodebuild -workspace "$tests.xcworkspace" -scheme "$tests" clean test | xcpretty -c
-popd
+pushd "$root_directory/Tests" > /dev/null
+echo "Updating CocoaPods..."
+pod update --silent
+xcodebuild -workspace "$tests.xcworkspace" -scheme "$tests" clean build | xcpretty -c
+xcodebuild -workspace "$tests.xcworkspace" -scheme "$tests" test
+popd > /dev/null
